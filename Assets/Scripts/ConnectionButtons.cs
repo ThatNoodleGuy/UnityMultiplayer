d9a79d2c30ea6a2e076c535871568f8c8cfa7589
Dor Nudel <dornu23@gmail.com>
2023-06-29 12:23:40 +0300

    Aiming

--- Assets/Scripts/ConnectionButtons.cs
+++ Assets/Scripts/ConnectionButtons.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
-public class JoinServer : MonoBehaviour
+public class ConnectionButtons : MonoBehaviour
 {
-    public void Join()
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartClient()
     {
         NetworkManager.Singleton.StartClient();
     }
